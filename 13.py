4e3b54464bb77ce7a37c77946f74ed4f0324f0ce
mimi <mimch1234@gmail.com>
2022-08-17 11:45:06 +0300

    add commit

--- 13.py
+++ 13.py
@@ -4,6 +4,10 @@ def save_name(names_file):
     my_file.write(current_name + "\n")
     my_file.close()
 
+def moshe(david):
+    print(david)
+
+
 def show_names(names_file):
     my_file = open(names_file,"r")
     for name in my_file.readlines():
